ab3b9d3511df2fa112e8a2bc6109208cde659c8b
saniyaj Mallik <saniyaj.mallik@wisdmlabs.com>
2025-05-18 11:39:34 +0530

    basic api

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // Server Do nothing but can server Tea onplay.
+
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
