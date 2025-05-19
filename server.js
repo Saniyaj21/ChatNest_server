da1ea9c203c43f07e34dedf87814e77ab31fcdd3
saniyaj Mallik <saniyaj.mallik@wisdmlabs.com>
2025-05-19 08:30:53 +0530

    socket added

--- server.js
+++ server.js
@@ -1,14 +1,68 @@
-// Server Do nothing but can server Tea onplay.
-
 import express from 'express';
+import 'dotenv/config';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+//--------------- imports end here -----------------------||
+
+
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+const server = http.createServer(app);
 
+// Middleware
+app.use(cors({
+  origin: "*",
+  exposedHeaders: ['X-Total-Count'],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "X-Total-Count"],
+}));
+app.use(express.json());
+
+//--------------- middleware end here --------------------||
+
+
+
+
+
+// Socket.io  
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+  transports: ['websocket', 'polling'],
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected', socket.id);
+});
+
+//--------------- socket.io end here ---------------------||
+
+
+
+
+
+
+// Home Routes
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(PORT, () => {
+//--------------- routes end here ------------------------||
+
+
+
+
+
+
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+//--------------- server start here ----------------------||
