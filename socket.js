cfa26b3dd2af3462791139ed10e8aa76cc1c0dd7
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-05-23 08:55:59 +0530

    global chat added

--- socket.js
+++ socket.js
@@ -9,11 +9,21 @@ export default function setupSocket(server) {
     transports: ['websocket', 'polling'],
   });
 
+  let activeUsers = 0;
   io.on('connection', (socket) => {
+    activeUsers++;
+    io.emit('active users', activeUsers);
     console.log('A user connected', socket.id);
     // You can add more socket event handlers here
 
+    socket.on('global message', (msg) => {
+      // Broadcast the message to all clients, including userId
+      io.emit('global message', { userId: msg.userId, text: msg.text });
+    });
+
     socket.on('disconnect', () => {
+      activeUsers = Math.max(0, activeUsers - 1);
+      io.emit('active users', activeUsers);
       console.log('A user disconnected', socket.id);
     });
   });
