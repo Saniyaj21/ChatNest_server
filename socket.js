109208ac55156c9853c6cdeed380ba949d3944eb
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-05-24 07:46:05 +0530

    active user count fixed

--- socket.js
+++ socket.js
@@ -8,17 +8,16 @@ export default function setupSocket(server) {
     },
     transports: ['websocket', 'polling'],
   });
-
-  let activeUsers = 0;
+  const activeConnections = new Set();
   io.on('connection', (socket) => {
-    activeUsers++;
-    io.emit('active users', activeUsers);
+    activeConnections.add(socket.id);
+    io.emit('activeUsers', activeConnections.size);
     console.log('A user connected', socket.id);
     // You can add more socket event handlers here
 
-    socket.on('global message', (msg) => {
+    socket.on('globalMessage', (msg) => {
       // Broadcast the message to all clients, including user info and timestamp
-      io.emit('global message', msg);
+      io.emit('globalMessage', msg);
     });
 
     socket.on('typing', (typingData) => {
@@ -26,14 +25,19 @@ export default function setupSocket(server) {
       socket.broadcast.emit('typing', typingData);
     });
 
+    socket.on('getActiveUsers', () => {
+      // Send current active users count to the requesting client
+      socket.emit('activeUsers', activeConnections.size);
+    });
+
     socket.on('disconnect', () => {
-      activeUsers = Math.max(0, activeUsers - 1);
-      io.emit('active users', activeUsers);
+      activeConnections.delete(socket.id);
+      io.emit('activeUsers', activeConnections.size);
       console.log('A user disconnected', socket.id);
     });
   });
 
-  
+
 
   return io;
-} 
+}
